7979d73ddfd3a65d8fa5eb1c76be3f5816b888d7
Gerson Lueders <gerson.lueders@gmail.com>
2022-01-12 12:49:29 -0300

    exemplos aula 1

---
+++ type-boolean.js
@@ -0,0 +1,7 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const terceiroNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+console.log(primeiroNumero === terceiroNumero);
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = '34567';
+
+const citacao = "O meu nome é ";
+const meuNome = 'Gerson';
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
